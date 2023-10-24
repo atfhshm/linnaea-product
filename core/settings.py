cb3e85ec94b1e57d59ccb852c305136dc4e889e6
atfhshm <its.atfhshm@gmail.com>
2023-10-24 14:11:08 +0300

    set the lifetime of the refresh token to 12 hours

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ SPECTACULAR_SETTINGS = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=12),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
